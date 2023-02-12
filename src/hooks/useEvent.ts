3ed7327b1b06247d495405383bcea0bd1df99a9e
jeonbyeongmin <qudals7613@gmail.com>
2023-02-12 15:10:03 +0900

    feat: useEvent hook

---
+++ src/hooks/useEvent.ts
@@ -0,0 +1,18 @@
+import { ReadEventPath, ReadEventReturn } from '@/api/events/read-event';
+
+import useSWR from 'swr';
+
+interface UseEventParams {
+  eventID: string;
+}
+
+export function useEvent({ eventID }: UseEventParams) {
+  const { data, isValidating } = useSWR<ReadEventReturn>(
+    eventID ? ReadEventPath({ eventID }) : null
+  );
+
+  return {
+    event: data?.event,
+    isLoading: isValidating,
+  };
+}
