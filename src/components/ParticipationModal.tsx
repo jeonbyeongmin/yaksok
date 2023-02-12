a07e8f673457fa45c98818653aba49a5d9f80c7b
jeonbyeongmin <qudals7613@gmail.com>
2023-02-12 17:52:15 +0900

    feat: participationModal

---
+++ src/components/ParticipationModal.tsx
@@ -0,0 +1,88 @@
+import {
+  Button,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+} from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+
+import { CreateParticipantAPI } from '@/api/participants/create-participant';
+
+interface ParticipationModalProps {
+  eventID: string;
+  eventTitle: string;
+  participantID: string;
+}
+
+function ParticipationModal({
+  eventID,
+  eventTitle,
+  participantID,
+}: ParticipationModalProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [name, setName] = useState<string>('');
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleButtonClick = async () => {
+    try {
+      const { participant } = await CreateParticipantAPI({
+        name,
+        eventID,
+      });
+
+      if (participant) {
+        setIsOpen(false);
+      }
+    } catch (error) {}
+  };
+
+  const handleModalClose = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    if (!participantID) {
+      setIsOpen(true);
+    }
+  }, [participantID]);
+
+  return (
+    <Modal
+      closeOnOverlayClick={false}
+      isOpen={isOpen}
+      isCentered
+      onClose={handleModalClose}
+    >
+      <ModalOverlay />
+      <ModalContent userSelect="none">
+        <ModalHeader>{`${eventTitle} 참여하기`}</ModalHeader>
+        <ModalBody pb={6}>
+          <Text mb={3} fontWeight="bold" color="gray.600">
+            이름
+          </Text>
+          <Input
+            value={name}
+            onChange={handleNameChange}
+            placeholder="이름을 입력해주세요"
+          />
+        </ModalBody>
+
+        <ModalFooter>
+          <Button fontSize="sm" onClick={handleButtonClick}>
+            확인
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export default ParticipationModal;
