142a14c52266170a3600b58477faf6e359111899
jeonbyeongmin <qudals7613@gmail.com>
2023-02-13 15:20:27 +0900

    feat: availableIndexes를 업데이트하는 api 함수 작성

---
+++ src/api/participants/update-participant.ts
@@ -0,0 +1,26 @@
+import { Participant } from 'server/inerfaces/Participant.interface';
+
+interface UpdateParticipantParams {
+  participantID: string;
+  availableIndexes: string[];
+}
+
+interface UpdateParticipantReturn {
+  success: boolean;
+  participant: Participant;
+}
+
+export const updateParticipant = async ({
+  participantID,
+  availableIndexes,
+}: UpdateParticipantParams): Promise<UpdateParticipantReturn> => {
+  const response = await fetch(`/api/participants/${participantID}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ availableIndexes }),
+  });
+
+  return response.json();
+};
