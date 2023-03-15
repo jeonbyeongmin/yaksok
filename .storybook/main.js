f20bdd3e2779e35ae723aba0533c48364a0240e0
Byeongmin Jeon <qudals7613@gmail.com>
2023-03-15 22:09:21 +0900

    chore: storybook/nextjs 로 마이그레이션 (#8)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,14 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-postcss',
+    '@storybook/addon-mdx-gfm',
   ],
-  framework: '@storybook/react',
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
 };
