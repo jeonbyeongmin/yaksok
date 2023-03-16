6de11aba05724f4e69ddf98c488a02e3222c2ac7
jeonbyeongmin <qudals7613@gmail.com>
2023-03-16 18:04:32 +0900

    style(storybook): support theme

---
+++ .storybook/preview.tsx
@@ -0,0 +1,25 @@
+// .storybook/preview.tsx
+
+import { Preview } from '@storybook/react';
+import React from 'react';
+import { ThemeProvider } from 'next-themes';
+
+const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableColorScheme={false}
+        value={{
+          light: 'light',
+          dark: 'dark-theme',
+        }}
+      >
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+};
+
+export default preview;
