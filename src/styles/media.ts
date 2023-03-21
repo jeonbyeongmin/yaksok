e30d7b2094d8fcc32aa913add69472de65306f45
jeonbyeongmin <qudals7613@gmail.com>
2023-03-21 13:07:50 +0900

    refactor(stitches): detach theme token from config file
    * create theme.type.ts

---
+++ src/styles/media.ts
@@ -0,0 +1,12 @@
+export const stitchesMedia = {
+  bp0: '(min-width: 0px)',
+  bp1: '(min-width: 480px)',
+  bp2: '(min-width: 768px)',
+  bp3: '(min-width: 1024px)',
+  bp4: '(min-width: 1280px)',
+
+  motion: '(prefers-reduced-motion)',
+  hover: '(any-hover: hover)',
+  dark: '(prefers-color-scheme: dark)',
+  light: '(prefers-color-scheme: light)',
+};
---
+++ src/styles/theme/typography.ts
@@ -0,0 +1,25 @@
+export const fontSizes = {
+  xs: '1.2rem',
+  sm: '1.4rem',
+  md: '1.6rem',
+  lg: '1.8rem',
+  xl: '2rem',
+  '2xl': '2.2rem',
+  '3xl': '2.4rem',
+};
+
+export const lineHeights = {
+  xs: '1.2',
+  sm: '1.3',
+  md: '1.4',
+  lg: '1.5',
+  xl: '1.6',
+  '2xl': '1.7',
+  '3xl': '1.8',
+};
+
+export const fontWeights = {
+  light: 300,
+  regular: 400,
+  bold: 700,
+};
