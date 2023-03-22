c336d5729c43e6ca52167bc422f1a2751d31dd0d
jeonbyeongmin <qudals7613@gmail.com>
2023-03-22 23:10:06 +0900

    fix: vercel i18n error

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     locales: ['default', 'en', 'ko'],
     defaultLocale: 'default',
+    localePath: path.resolve('./public/static/locales'),
     // localeDetection: false,
   },
 };
