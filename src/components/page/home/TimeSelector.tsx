293c42496e9bd4cc3611dd7c54d25cbad092db42
jeonbyeongmin <qudals7613@gmail.com>
2023-03-30 19:23:42 +0900

    refactor: detach reducer, action from event form

--- src/components/page/home/TimeSelector.tsx
+++ src/components/page/home/TimeSelector.tsx
@@ -19,7 +19,7 @@ interface TimeSelectorProps {
 const am = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'];
 const pm = ['12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'];
 
-function TimeSelector({ handleValue, value, enableTime }: TimeSelectorProps) {
+export function TimeSelector({ handleValue, value, enableTime }: TimeSelectorProps) {
   const { t } = useTranslation(['common', 'home-page']);
   return (
     <Select onValueChange={handleValue} value={value}>
@@ -64,5 +64,3 @@ const LabelWrapper = styled(Box, {
   p: '$4 $7',
   color: '$gray500',
 });
-
-export default TimeSelector;
