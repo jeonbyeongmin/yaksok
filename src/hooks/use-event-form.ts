85ea0a03a7a06f32f62c21cb049f5ae2bd8d2c96
jeonbyeongmin <qudals7613@gmail.com>
2023-05-11 04:20:56 +0900

    refactor: useEventForm 수정

--- src/hooks/use-event-form.ts
+++ src/hooks/use-event-form.ts
@@ -1,32 +1,20 @@
-import { ChangeEvent, useReducer, useState } from 'react';
-import {
-  eventFormInitialState,
-  eventFormReducer,
-} from '@/reducers/event-form.reducer';
-
-import type { DateType } from '@/components/page/home/Calendar';
+import type { DateType } from '@/components/page/home/calendar';
 import type { TFunction } from 'next-i18next';
-import { createEventAPI } from '@/api/events/create-event';
-import { createParticipantAPI } from '@/api/participants/create-participant';
-import { eventFormActions } from '@/actions/event-form.action';
+
 import { logOnBrowser } from 'common/utils/log';
+import { ChangeEvent, useReducer, useState } from 'react';
+
+import { eventFormActions } from '@/actions/event-form.action';
+import { createEventAPI } from '@/api/events/create-event';
+import { eventFormInitialState, eventFormReducer } from '@/reducers/event-form.reducer';
 
 export const useEventForm = ({ t }: { t: TFunction }) => {
-  const [eventForm, dispatch] = useReducer(
-    eventFormReducer,
-    eventFormInitialState,
-  );
+  const [eventForm, dispatch] = useReducer(eventFormReducer, eventFormInitialState);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string>('');
 
   const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
     dispatch(eventFormActions.changeTitle(e.target.value));
-  };
-  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    dispatch(eventFormActions.changeName(e.target.value));
-  };
-  const handleParticipantsNumberChange = (participantsNumber: string) => {
-    dispatch(eventFormActions.changeParticipantsNumber(participantsNumber));
   };
   const handleDateChange = (date: DateType) => {
     dispatch(eventFormActions.changeDate(date));
@@ -39,12 +27,8 @@ export const useEventForm = ({ t }: { t: TFunction }) => {
   };
 
   const validate = () => {
-    const { title, name, participantsNumber, date, startTime, endTime } =
-      eventForm;
+    const { title, date, startTime, endTime } = eventForm;
     if (!title) throw new Error(t('common:error.change-event-title'));
-    if (!name) throw new Error(t('common:error.change-event-name'));
-    if (!participantsNumber)
-      throw new Error(t('common:error.change-event-number'));
     if (!date) throw new Error(t('common:error.change-event-date'));
     if (!startTime) throw new Error(t('common:error.change-event-start-time'));
     if (!endTime) throw new Error(t('common:error.change-event-end-time'));
@@ -57,7 +41,6 @@ export const useEventForm = ({ t }: { t: TFunction }) => {
       startDate,
       endDate,
       title: eventForm.title,
-      participantsNumber: Number(eventForm.participantsNumber),
       startTime: Number(eventForm.startTime),
       endTime: Number(eventForm.endTime) - 1,
     };
@@ -67,18 +50,12 @@ export const useEventForm = ({ t }: { t: TFunction }) => {
     return event._id;
   };
 
-  const createParticipant = async (eventId: string) => {
-    const { name } = eventForm;
-    await createParticipantAPI({}, { name, eventId, availableIndexes: [] });
-  };
-
   const handleEventCreate = async () => {
     try {
       validate();
       setIsLoading(true);
 
       const eventId = await createEvent();
-      await createParticipant(eventId);
 
       return eventId;
     } catch (error) {
@@ -95,8 +72,6 @@ export const useEventForm = ({ t }: { t: TFunction }) => {
     isLoading,
     error,
     handleTitleChange,
-    handleNameChange,
-    handleParticipantsNumberChange,
     handleDateChange,
     handleStartTime,
     handleEndTime,
