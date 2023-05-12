04c247e30de22923149afedae86f322069639759
Byeongmin Jeon <qudals7613@gmail.com>
2023-05-12 17:54:47 +0900

    fix: removed files

--- src/components/primitive/Select/index.ts
+++
@@ -1,6 +0,0 @@
-import { SelectItem } from '@/components/primitive/select/select-item';
-import { SelectSeparator } from '@/components/primitive/select/select-separator';
-import { SelectWrapper } from '@/components/primitive/select/select-wrapper';
-import { SelectGroup, SelectLabel } from '@radix-ui/react-select';
-
-export { SelectItem, SelectWrapper as Select, SelectGroup, SelectLabel, SelectSeparator };
