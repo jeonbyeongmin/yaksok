250cf0b2531287cb5cab2a0c0733dbe5180ec8de
jeonbyeongmin <qudals7613@gmail.com>
2023-05-12 16:03:40 +0900

    refactor: divide use-timetable features

--- src/utils/timetable.ts
+++ src/utils/timetable.ts
@@ -1,4 +1,9 @@
-function convertTableToIndexStrings(table: boolean[][]) {
+import type { Event } from 'common/inerfaces/Event.interface';
+import type { Participant } from 'common/inerfaces/Participant.interface';
+
+import dayjs from 'dayjs';
+
+export function convertTableToIndexStrings(table: boolean[][]) {
   const array: string[] = [];
 
   table.forEach((row, rowIndex) => {
@@ -12,4 +17,37 @@ function convertTableToIndexStrings(table: boolean[][]) {
   return array;
 }
 
-export { convertTableToIndexStrings };
+export function generateTimetable({
+  event,
+  participants,
+}: {
+  event: Event;
+  participants: Participant[] | Participant | undefined;
+}) {
+  const { startDate, endDate, startTime, endTime } = event;
+  const timetable: number[][] = Array.from(Array((endTime - startTime + 1) * 2), () =>
+    new Array(dayjs(endDate).diff(dayjs(startDate), 'day') + 1).fill(0),
+  );
+
+  if (participants) {
+    if (Array.isArray(participants)) {
+      participants.forEach((participant) => {
+        participant.availableIndexes.forEach((availableIndex) => {
+          const [rowIndex, colIndex] = availableIndex.split('-').map(Number);
+          timetable[rowIndex][colIndex] += 1;
+        });
+      });
+    } else {
+      participants.availableIndexes.forEach((availableIndex) => {
+        const [rowIndex, colIndex] = availableIndex.split('-').map(Number);
+        timetable[rowIndex][colIndex] += 1;
+      });
+    }
+  }
+
+  return timetable;
+}
+
+export function convertNumberTableToBooleanTable(table: number[][]) {
+  return table.map((row) => row.map((column) => column > 0));
+}
