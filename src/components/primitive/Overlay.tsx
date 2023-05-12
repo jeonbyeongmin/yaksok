7e0c89f232321b8cf77719cebdce37a52261fd02
Byeongmin Jeon <qudals7613@gmail.com>
2023-05-12 17:49:40 +0900

    fix: removed file bug

--- src/components/primitive/Overlay.tsx
+++
@@ -1,8 +0,0 @@
-import { css, styled } from '@/styles/stitches.config';
-
-export const overlayStyles = css({
-  bgColor: 'rgba(0, 0, 0, .6)',
-  backdropFilter: 'blur(8px)',
-});
-
-export const Overlay = styled('div', overlayStyles);
